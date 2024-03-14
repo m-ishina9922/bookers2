7f9a4100922bcdd76bfdc20483a075b32fd53200
EC2 Default User <ec2-user@ip-172-31-18-92.ap-northeast-1.compute.internal>
2024-03-14 07:31:03 +0000

    rspec last

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :move_to_signed_in, only: [:edit, :show, :index]
   def new
    @book = Book.new
   end
@@ -16,22 +17,33 @@ class BooksController < ApplicationController
   end
 
   def index
+     move_to_signed_in
     @book = Book.new
     @books = Book.all
   end
 
   def show
+     move_to_signed_in
     @book = Book.find(params[:id])
   end
 
   def edit
+     move_to_signed_in
     @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      @books = Book.all
+      redirect_to '/books'
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -43,5 +55,12 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+   private
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,44 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :move_to_signed_in, only: [:edit, :update, :show, :index]
 
   def show
+    move_to_signed_in
     @user = User.find(params[:id])
     @users = @user.books
   end
 
   def edit
+    move_to_signed_in
     @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
+    move_to_signed_in
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path
+    if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
+       redirect_to user_path
+    else
+       render 'edit'
+    end
   end
 
   def index
+    move_to_signed_in
     @user =User.all
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image, :introduct)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to books_path
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
     end
   end
 end
